a5d9e00fe4f12cc34fe8f75c4608245393cb422a
Kostas Vs <kostasvs@live.com>
2024-01-14 03:48:26 +0200

    switch interactable

---
+++ Assets/Scripts/SwitchInteractable.cs
@@ -0,0 +1,70 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.Input;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts {
+	public class SwitchInteractable : MonoBehaviour {
+
+		private Hand lastHand;
+		private PokeInteractable pokeInteractable;
+
+		[SerializeField]
+		private float releaseDistance = 0.03f;
+
+		[SerializeField]
+		private bool actuateHorizontal = true;
+		[SerializeField]
+		private bool actuateVertical = true;
+
+		public enum SwitchDirection {
+			Up,
+			Down,
+			Left,
+			Right
+		}
+		public UnityEvent<SwitchDirection> OnSwitchActuate = new UnityEvent<SwitchDirection> ();
+
+		void Start () {
+			pokeInteractable = GetComponent<PokeInteractable> ();
+			var wrapper = GetComponent<InteractableUnityEventWrapper> ();
+			wrapper.WhenSelect.AddListener (OnSelect);
+			wrapper.WhenUnselect.AddListener (OnUnselect);
+
+			pokeInteractable.InjectOptionalReleaseDistance (releaseDistance);
+		}
+
+		private void OnSelect () {
+			foreach (var i in pokeInteractable.Interactors) {
+				var handRef = i.GetComponent<HandRef> ();
+				if (handRef && handRef.Hand != null && handRef.Hand is Hand hand) {
+					lastHand = hand;
+					break;
+				}
+			}
+		}
+
+		private void OnUnselect () {
+			if (lastHand == null) return;
+			if (lastHand.GetJointPose (HandJointId.HandIndexTip, out Pose jointPose)) {
+				var delta = transform.InverseTransformPointUnscaled (jointPose.position);
+				if (actuateHorizontal && Mathf.Abs (delta.x) > Mathf.Max (Mathf.Abs (delta.y), -delta.z)) {
+					if (delta.x > 0) {
+						OnSwitchActuate.Invoke (SwitchDirection.Right);
+					}
+					else {
+						OnSwitchActuate.Invoke (SwitchDirection.Left);
+					}
+				}
+				else if (actuateVertical && Mathf.Abs (delta.y) > Mathf.Max (Mathf.Abs (delta.x), -delta.z)) {
+					if (delta.y > 0) {
+						OnSwitchActuate.Invoke (SwitchDirection.Up);
+					}
+					else {
+						OnSwitchActuate.Invoke (SwitchDirection.Down);
+					}
+				}
+			}
+		}
+	}
+}
